b759dd61bd980e828672d02c4e5f9cecec20f464
Fikret Kurtiev <fikretkurtiev@gmail.com>
2016-11-02 20:11:10 +0200

    app init

---
+++ app/directives/navigation/navigation.js
@@ -0,0 +1,20 @@
+(function () {
+
+    'use strict';
+
+    var link = function (scope, element, attrs) {
+        console.log('wefew')
+    };
+
+    var navigation = function () {
+        return {
+            templateUrl: 'app/directives/navigation/template.html',
+            restrict: 'E',
+            link: link
+        };
+    };
+
+
+    angular.module('app.directives').directive('navigation', navigation);
+
+})();
--- app/pages/home/home.controller.js
+++ app/pages/home/home.controller.js
@@ -3,7 +3,7 @@
     'use strict';
 
     var homeController = function ($state) {
-        console.log($state);
+
     };
 
 
